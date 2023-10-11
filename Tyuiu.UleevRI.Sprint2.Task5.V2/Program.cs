098122af611a09050684d578b0d3b900af8cdb2e
Tyuiu.UleevRI@outlook.com <romanuleev178@gmail.com>
2023-10-12 01:48:59 +0500

    Task5

---
+++ Tyuiu.UleevRI.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.UleevRI.Sprint2.Task5.V2.Lib;
+
+namespace Tyuiu.UleevRI.Sprint2.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Улеев Р.И. | ИИПБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант 2                                                               *");
+            Console.WriteLine("* Выполнил: Улеев Роман Игоревич | ИИПБ-23-3                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("* Условие: По данному месяцу, определите в какую пору года попадает этот  *");
+            Console.WriteLine("* месяц(Зима, Лето, Весна, Осень).                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер месяца: ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+            string res;
+            if ((numMouth < 1) || (numMouth > 12)) res = "Введено неверное значение!";
+            else res = "Это сезон года: " + ds.FindMonthSeason(numMouth);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
